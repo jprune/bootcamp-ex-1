2a223d5b905abfb786f66539cda0dc758b64927c
jp rune <105754345+jprune@users.noreply.github.com>
2022-06-25 00:14:03 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'bkg-header': "url('/resources/design-assets/bgk-header.png')",
+        'bkg-middle': "url('/resources/design-assets/bgk-middle.png')",
+        'bkg-footer': "url('/resources/design-assets/bgk-footer.png')",
+      }
+    },
+  },
+  plugins: [],
+}
